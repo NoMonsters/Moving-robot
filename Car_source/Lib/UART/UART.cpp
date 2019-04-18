c3b96e854038ef8b818c1a73bd801cf69ca50620
NoMonsters <mlx-01@yandex.ru>
2019-04-18 18:27:39 +0300

    Update UART.cpp
    Добавлена функция чтения UART исправлены ошибки

--- Car_source/Lib/UART/UART.cpp
+++ Car_source/Lib/UART/UART.cpp
@@ -6,6 +6,7 @@ void UART_init()
 	
 	UBRR0L = UBRRL_value;							//Младшие 8 бит UBRRL_value
 	UBRR0H = (UBRRL_value>>8);						//Старшие 8 бит UBRRL_value
+	UCSR0A |=(1 << U2X0);							//Установка двойной скорости
 	UCSR0B |=(1<<TXEN0)|(1<<RXEN0);					//Бит разрешения передачи и бит разрешения приема
 	UCSR0C |=(1<< UCSZ00)|(1<< UCSZ01);				//Устанавливем формат 8 бит данных
 }
@@ -15,6 +16,13 @@ void UART_send_char(char value)
 	while(!( UCSR0A & (1 << UDRE0)));   // Ожидаем когда очистится буфер передачи
 	UDR0 = value;						// Помещаем данные в буфер, начинаем передачу
 }
+
+char UART_get_char()
+{
+	while(!( UCSR0A  & (1 << RXC0)));   // Ожидаем когда очистится буфер передачи
+	return UDR0;						// Помещаем данные в буфер, начинаем передачу
+}
+
 
 void UART_send_Str(char *str)
 {
